8aaf673d047e00124f699f96728fcb99783a4b15
unknown <matt1988@gmail.com>
2013-06-24 08:49:04 -0600

    Started integration with joda time APIs

--- EventTime.java
+++ EventTime.java
@@ -1,11 +1,19 @@
 package com.mjknox.gdw;
 
+import java.util.TimeZone;
+
 import org.json.simple.JSONObject;
+import org.joda.time.*;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 public class EventTime {
 	private String text;
 	private Long value;
-	private String timeZone;
+	private DateTimeZone timeZone;
+	private DateTime time;
+	private DateTimeFormatter fmt = DateTimeFormat.forPattern("D y h:ma");
+
 	
 	/**
 	 * Creates an event time object generated from a JSONObject
@@ -14,8 +22,9 @@ public class EventTime {
 	public EventTime(JSONObject jsonData){
 		this.text = (String) jsonData.get("text");
 		this.value = (Long) jsonData.get("value");
-		this.timeZone = (String) jsonData.get("time_zone");
-
+		this.timeZone = DateTimeZone.forID((String) jsonData.get("time_zone"));
+		this.time = DateTime.parse(DateTime.now().getDayOfYear()+" "+DateTime.now().getYear()+" "+this.text,fmt);
+		
 	}
 	
 	/**
@@ -35,7 +44,14 @@ public class EventTime {
 	/**
 	 * @return the timezone that this time is in.
 	 */
-	public String getTimeZone() {
+	public DateTimeZone getTimeZone() {
 		return timeZone;
+	}
+	
+	/**
+	 * @return the time as a Joda Instant.
+	 */
+	public DateTime getTime() {
+		return time;
 	}
 }
